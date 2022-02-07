962b43f96de14189244508ceb21d21b1e93bb550
RakeshKumarParida <rakeshkumarparida424@gmail.com>
2022-02-07 09:38:55 +0530

    Added Loops

---
+++ loops.cpp
@@ -0,0 +1,60 @@
+// For while Lopp
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // print Hello World 5 times using for loop
+
+    /*
+    for (int i = 1; i <= 5; i++){
+        cout << "Hello World " << i <<endl;
+    }
+    */
+
+    // print Hello World 5 times using while loop
+
+    /*
+        int i = 1;
+
+        while(i <= 5){
+            cout << "Hello World " << i << endl;
+            i++;
+        };
+
+    */
+
+    // Print Hello World 5 times using do while loop
+
+
+/*
+    int i = 1;
+    do
+    {
+        cout << "Hello World " << i << endl;
+        i++;
+    } while (i <= 5);
+
+
+    // here above example conditon is true however when it false it would print atleast one time 
+
+
+    */
+
+                                    // infinite Loop
+
+                                    
+    /*
+    int i = 0;
+
+    while(true){
+        cout << "Hello World " << i << endl;
+        i++;
+    }
+
+    return 0;
+
+    */
+}
